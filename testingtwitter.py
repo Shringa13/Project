e1e944665cc3bf6da0cf384024c064d7ddb7b4fa
Shringa13 <sbais1@hawk.iit.edu>
2016-09-04 20:35:30 +0530

    Create testingtwitter.py

---
+++ testingtwitter.py
@@ -0,0 +1,19 @@
+# Imports you'll need.
+from collections import Counter
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+import time
+import twitter
+
+
+  api = twitter.Api(consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ',
+  consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u',
+  access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv',
+  access_token_secret = 'o1U7MBSrB5MCVCO3mvSb860qoH7JexsYP98TjsjwjbRsX')
+
+
+ 
+
+
+
