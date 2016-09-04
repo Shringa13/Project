7e4e40abe881d249c8a352e7cab2ee25c2ef2881
Shringa13 <sbais1@hawk.iit.edu>
2016-09-04 20:37:57 +0530

    Update testingtwitter.py

--- testingtwitter.py
+++ testingtwitter.py
@@ -6,11 +6,9 @@ import sys
 import time
 import twitter
 
-
-  api = twitter.Api(consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ',
-  consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u',
-  access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv',
-  access_token_secret = 'o1U7MBSrB5MCVCO3mvSb860qoH7JexsYP98TjsjwjbRsX')
+api = twitter.Api(consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ',consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u',access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv',access_token_secret = 'o1U7MBSrB5MCVCO3mvSb860qoH7JexsYP98TjsjwjbRsX')
+statuses = api.GetUserTimeline(user)
+print([s.text for s in statuses])
 
 
  
