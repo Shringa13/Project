9590e2db6b808c0e234c3365b5e8653f9dfd36f4
Shringa13 <sbais1@hawk.iit.edu>
2016-09-04 15:21:09 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -37,6 +37,29 @@ def read_screen_names(filename):
     """
     fh=open(filename,"r")
     return fh.read()
+    
+ # I've provided the method below to handle Twitter's rate limiting.
+# You should call this method whenever you need to access the Twitter API.
+def robust_request(twitter, resource, params, max_tries=5):
+    """ If a Twitter request fails, sleep for 15 minutes.
+    Do this at most max_tries times before quitting.
+    Args:
+      twitter .... A TwitterAPI object.
+      resource ... A resource string to request; e.g., "friends/ids"
+      params ..... A parameter dict for the request, e.g., to specify
+                   parameters like screen_name or count.
+      max_tries .. The maximum number of tries to attempt.
+    Returns:
+      A TwitterResponse object, or None if failed.
+    """
+    for i in range(max_tries):
+        request = twitter.request(resource, params)
+        if request.status_code == 200:
+            return request
+        else:
+            print('Got error %s \nsleeping for 15 minutes.' % request.text)
+            sys.stderr.flush()
+            time.sleep(61 * 15)
   
 def main():
     """ Main method. You should not modify this. """
