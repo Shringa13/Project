46a9fabcf10a90f50e615814bd40158b9b9569ee
Shringa13 <sbais1@hawk.iit.edu>
2016-09-04 20:32:35 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,10 +7,9 @@ import time
 import twitter
 
 
- api = twitter.Api(
-consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ'
-consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u'
-access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv'
+ api = twitter.Api(consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ',
+consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u',
+access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv',
 access_token_secret = 'o1U7MBSrB5MCVCO3mvSb860qoH7JexsYP98TjsjwjbRsX')
 
 def read_screen_names(filename):
