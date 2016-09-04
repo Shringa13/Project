09d203b54215ad871aa967140c92300125a067e0
Shringa13 <sbais1@hawk.iit.edu>
2016-09-04 21:31:38 +0530

    Update testingtwitter.py

--- testingtwitter.py
+++ testingtwitter.py
@@ -4,11 +4,72 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import sys
 import time
-import twitter
+from TwitterAPI import TwitterAPI
 
-api = twitter.Api(consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ',consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u',access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv',access_token_secret = 'o1U7MBSrB5MCVCO3mvSb860qoH7JexsYP98TjsjwjbRsX')
-statuses = api.GetUserTimeline(user)
-print([s.text for s in statuses])
+
+consumer_key = 'cgzXf3uv155176JFvWFDPPTLQ'
+consumer_secret = 'XF7sNj92qfgB5RvM4FDEwk6bJEVjnPoQDM4ClZ8BVRqfVXA21u'
+access_token = '771404469372203008-LuQQIosZXnxSzTZKf8gZx2J13qalQKv'
+access_token_secret = 'o1U7MBSrB5MCVCO3mvSb860qoH7JexsYP98TjsjwjbRsX'
+
+
+# This method is done for you. Make sure to put your credentials in the file twitter.cfg.
+def get_twitter():
+    """ Construct an instance of TwitterAPI using the tokens you entered above.
+    Returns:
+      An instance of TwitterAPI.
+    """
+    return TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+
+
+
+def get_users(twitter, screen_names):
+    """Retrieve the Twitter user objects for each screen_name.
+    Params:
+        twitter........The TwitterAPI object.
+        screen_names...A list of strings, one per screen_name
+    Returns:
+        A list of dicts, one per user, containing all the user information
+        (e.g., screen_name, id, location, etc)
+    See the API documentation here: https://dev.twitter.com/rest/reference/get/users/lookup
+    In this example, I test retrieving two users: twitterapi and twitter.
+    >>> twitter = get_twitter()
+    >>> users = get_users(twitter, ['twitterapi', 'twitter'])
+    >>> [u['id'] for u in users]
+    [6253282, 783214]
+    """
+    users = twitter._RestIterable(screen_names)
+    print('Established Twitter connection.')
+  
+
+def main():
+    """ Main method. You should not modify this. """
+    twitter = get_twitter()
+    u= get_users(twitter,'Shringa13')
+    """
+    screen_names = read_screen_names('candidates.txt')
+    print('Established Twitter connection.')
+    print('Read screen names: %s' % screen_names)
+    
+    users = sorted(get_users(twitter, screen_names), key=lambda x: x['screen_name'])
+    print('found %d users with screen_names %s' %
+          (len(users), str([u['screen_name'] for u in users])))
+    add_all_friends(twitter, users)
+    print('Friends per candidate:')
+    print_num_friends(users)
+    friend_counts = count_friends(users)
+    print('Most common friends:\n%s' % str(friend_counts.most_common(5)))
+    print('Friend Overlap:\n%s' % str(friend_overlap(users)))
+    print('User followed by Hillary and Donald: %s' % followed_by_hillary_and_donald(users, twitter))
+
+    graph = create_graph(users, friend_counts)
+    print('graph has %s nodes and %s edges' % (len(graph.nodes()), len(graph.edges())))
+    draw_network(graph, users, 'network.png')
+    print('network drawn to network.png')
+"""
+
+if __name__ == '__main__':
+    main()
 
 
  
