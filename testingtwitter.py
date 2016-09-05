7ce12fff2bd3047642e77f28471c1416d3094fae
Shringa13 <sbais1@hawk.iit.edu>
2016-09-05 19:10:22 +0530

    Update testingtwitter.py

--- testingtwitter.py
+++ testingtwitter.py
@@ -91,6 +91,7 @@ def get_users(twitter, screen_names):
         print (screen_names[sn])
     """
     request=robust_request(twitter,'users/lookup', {'screen_name':screen_names})
+        return request
     """
     params = [i for i in range(len(screen_names))]
     print (params)
@@ -106,11 +107,13 @@ def main():
     screen_names = read_screen_names('candidates.txt')
     print('Established Twitter connection.')
     print('Read screen names: %s' % screen_names)
-    get_users(twitter, screen_names)
-    users = sorted(get_users(twitter, screen_names), key=lambda x: x['screen_name'])
+    users = get_users(twitter, screen_names)
     print('found %d users with screen_names %s' %
           (len(users), str([u['screen_name'] for u in users])))
     """
+    users = sorted(get_users(twitter, screen_names), key=lambda x: x['screen_name'])
+    print('found %d users with screen_names %s' %
+          (len(users), str([u['screen_name'] for u in users])))
     add_all_friends(twitter, users)
     print('Friends per candidate:')
     print_num_friends(users)
