3b02a21632c75b7dace65abaa713c9358349429b
Shringa13 <sbais1@hawk.iit.edu>
2016-09-05 16:48:00 +0530

    Update testingtwitter.py

--- testingtwitter.py
+++ testingtwitter.py
@@ -78,8 +78,8 @@ def get_users(twitter, screen_names):
     [6253282, 783214]
   
     """
-    for name in screen_names:
-        print(name.strip())
+  
+        print(screen_names.strip())
     for sn in range(len(screen_names)):
         print (screen_names[sn])
     """request=robust_request(twitter,'users/lookup','screen_name:'screen_names)
