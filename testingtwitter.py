4528bbeff5de9e70b390e2af8d8089df5aa4cc1e
Shringa13 <sbais1@hawk.iit.edu>
2016-09-05 18:55:28 +0530

    Update testingtwitter.py

--- testingtwitter.py
+++ testingtwitter.py
@@ -37,11 +37,10 @@ def read_screen_names(filename):
     """
     f=open(filename,"r")
     scrn= f.readlines()
-    """
     for sn in range(len(scrn)):
     	scrn[sn].strip()
     return scrn
-    """
+
 
 # I've provided the method below to handle Twitter's rate limiting.
 # You should call this method whenever you need to access the Twitter API.
